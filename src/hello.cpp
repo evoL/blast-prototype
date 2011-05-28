d7dbf35fc33e6edc3d85b5605e5dcf0d5c326130
Rafał Hirsz <hirszdesign@gmail.com>
2011-05-28 20:28:23 +0200

    Hello World na SDL

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,41 @@
-#include <iostream>
+#include "configuration.h"
+#include "SDL.h"
+#include "SDL_image.h"
 
-int main (int argc, char const *argv[])
-{
-    std::cout << "Hello World" << std::endl;
+#include <string>
+
+SDL_Surface* loadImage(std::string filename) {
+    SDL_Surface* loadedImage = NULL;
+    SDL_Surface* optimizedImage = NULL;
+    
+    loadedImage = IMG_Load(filename.c_str());
+    
+    if (loadedImage != NULL) {
+        optimizedImage = SDL_DisplayFormat(loadedImage);
+        
+        SDL_FreeSurface(loadedImage);
+    }
+    
+    return optimizedImage;
+}
+
+int main (int argc, char* argv[]) {
+    // surfacy
+    SDL_Surface* hello = NULL;
+    SDL_Surface* screen = NULL;
+    
+    SDL_Init(SDL_INIT_EVERYTHING);
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+    
+    hello = loadImage(projectPath("dziala.png"));
+    
+    SDL_BlitSurface(hello, NULL, screen, NULL);
+    SDL_Flip(screen);
+    
+    SDL_Delay(5000);
+    
+    SDL_FreeSurface(hello);
+    SDL_Quit();
+    
     return 0;
 }
