ef7bc1c9dc490e171115585d222a99832f8c19a5
Rafał Hirsz <hirszdesign@gmail.com>
2011-05-28 22:49:02 +0200

    Da się wyłączyć na X!

--- src/hello.cpp
+++ src/hello.cpp
@@ -4,6 +4,12 @@
 
 #include <string>
 
+const int SCREEN_WIDTH  = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP    = 32;
+
+SDL_Surface* screen = NULL;
+
 SDL_Surface* loadImage(std::string filename) {
     SDL_Surface* loadedImage = NULL;
     SDL_Surface* optimizedImage = NULL;
@@ -19,20 +25,43 @@ SDL_Surface* loadImage(std::string filename) {
     return optimizedImage;
 }
 
+void applySurface(int x, int y, SDL_Surface* source, SDL_Surface* destination) {
+    SDL_Rect offset;
+    offset.x = x;
+    offset.y = y;
+    
+    SDL_BlitSurface(source, NULL, destination, &offset);
+}
+
+bool init() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) return false;
+    
+    screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
+    if (screen == NULL) return false;
+    
+    SDL_WM_SetCaption("Cuboid", NULL);
+    return true;
+}
+
 int main (int argc, char* argv[]) {
     // surfacy
     SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
+    SDL_Event event;
+    bool quit = false;
     
-    SDL_Init(SDL_INIT_EVERYTHING);
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
-    
+    if (!init()) return 1;
     hello = loadImage(projectPath("dziala.png"));
+    applySurface(0, 0, hello, screen);
     
-    SDL_BlitSurface(hello, NULL, screen, NULL);
-    SDL_Flip(screen);
+    if (SDL_Flip(screen) == -1) return 1;
     
-    SDL_Delay(5000);
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+    }
     
     SDL_FreeSurface(hello);
     SDL_Quit();
